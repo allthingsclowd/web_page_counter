61364dace8ab52238e3d6b2d46bd96fdddea83c7
Graham Land <graham@allthingscloud.eu>
2018-05-24 20:11:27 +0100

    applied versions to packages

--- go_init.sh
+++ go_init.sh
@@ -8,7 +8,7 @@ fi
 touch /var/vagrant_go_base
 sudo apt-get update && \
     sudo apt-get upgrade -y && \
-    sudo apt-get install -y git
+    sudo apt-get install -y git=1:2.7.4-0ubuntu1.3
 mkdir -p /tmp/go_src
 pushd /tmp/go_src
 wget -nv https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
