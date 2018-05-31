8e565bd6908002ed56f668c7523dd870fa46509b
Graham Land <graham@allthingscloud.eu>
2018-05-31 10:11:31 +0100

    removed apt upgrades

--- scripts/webserver_install.sh
+++ scripts/webserver_install.sh
@@ -7,9 +7,8 @@ if [ -f "/var/vagrant_web_server" ]; then
 fi
 
 touch /var/vagrant_web_server
-sudo apt-get update && \
-    sudo apt-get upgrade -y && \
-    sudo apt-get install -y nginx=1.10.3-0ubuntu0.16.04.2
+sudo apt-get update
+sudo apt-get install -y nginx=1.10.3-0ubuntu0.16.04.2
 sudo rm /etc/nginx/sites-enabled/default
 sed -i 's/GO_DEV_IP/'"$GO_DEV_IP"'/' nginx.conf
 sed -i 's/GO_DEV_GUEST_PORT/'"$GO_GUEST_PORT"'/' nginx.conf
