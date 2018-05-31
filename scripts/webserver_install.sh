5f807fbebca3c18007d1463031df18a023ad0f95
Graham Land <graham@allthingscloud.eu>
2018-05-31 10:32:16 +0100

    wip

--- scripts/webserver_install.sh
+++ scripts/webserver_install.sh
@@ -10,10 +10,10 @@ touch /var/vagrant_web_server
 sudo apt-get update
 sudo apt-get install -y nginx=1.10.3-0ubuntu0.16.04.2
 sudo rm /etc/nginx/sites-enabled/default
-sed -i 's/GO_DEV_IP/'"$GO_DEV_IP"'/' nginx.conf
-sed -i 's/GO_DEV_GUEST_PORT/'"$GO_GUEST_PORT"'/' nginx.conf
-sed -i 's/NGINX_GUEST_PORT/'"$NGINX_GUEST_PORT"'/' nginx.conf
 sudo cp /usr/local/bootstrap/conf/nginx.conf /etc/nginx/sites-available/default
+sed -i 's/GO_DEV_IP/'"$GO_DEV_IP"'/' /etc/nginx/sites-available/default
+sed -i 's/GO_DEV_GUEST_PORT/'"$GO_GUEST_PORT"'/' /etc/nginx/sites-available/default
+sed -i 's/NGINX_GUEST_PORT/'"$NGINX_GUEST_PORT"'/' /etc/nginx/sites-available/default
 sudo chmod 777 /etc/nginx/sites-available/default
 sudo chown root:root /etc/nginx/sites-available/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
