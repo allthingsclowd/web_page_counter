ed6e4e0b39d9b971a982115b52892f0a9b845cf5
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-06-01 13:07:54 +0200

    add sleep to consul

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -24,10 +24,14 @@ if [[ "${HOSTNAME}" =~ "consul" ]]; then
   echo server
   /usr/local/bin/consul members 2>/dev/null || {
     /usr/local/bin/consul agent -server -ui -client=0.0.0.0 -bind=${IP} -data-dir=/usr/local/consul -bootstrap-expect=1 >/vagrant/consul_${HOSTNAME}.log &
+    sleep 1
   }
 else
   echo agent
   /usr/local/bin/consul members 2>/dev/null || {
     /usr/local/bin/consul agent -bind=${IP} -data-dir=/usr/local/consul -join=192.168.2.11 >/vagrant/consul_${HOSTNAME}.log &
+    sleep 1
   }
 fi
+    
+echo consul started
