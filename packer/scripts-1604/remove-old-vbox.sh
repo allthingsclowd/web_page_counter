18bd1745f03da0c1ab6ae028254dc75127791c4a
Graham Land <graham@allthingscloud.eu>
2018-06-02 22:32:56 +0100

    Packer file modified and uploaded to vagrant cloud

---
+++ packer/scripts-1604/remove-old-vbox.sh
@@ -0,0 +1,15 @@
+# Without libdbus virtualbox would not start automatically after compile
+apt-get -y install --no-install-recommends libdbus-1-3
+
+# Install Linux headers and compiler toolchain
+apt-get -y install build-essential linux-headers-$(uname -r)
+
+# The netboot installs the VirtualBox support (old) so we have to remove it
+service virtualbox-ose-guest-utils stop
+rmmod vboxguest
+apt-get purge -y virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+apt-get install -y dkms
+
+echo 'Added this reboot as test'
+shutdown -r now
+sleep 60
