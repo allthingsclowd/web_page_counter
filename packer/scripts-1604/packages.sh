4871060678eb1ca589c870cb47e7007a2ab0cb33
Graham Land <graham@allthingscloud.eu>
2018-06-03 02:09:20 +0100

    rerun packer due to missing binaries

--- packer/scripts-1604/packages.sh
+++ packer/scripts-1604/packages.sh
@@ -10,6 +10,31 @@ apt-get remove -y plymouth-theme-ubuntu-text
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
 update-grub
 
+apt-get install -y wget unzip git redis-server nginx
+
+which /usr/local/bin/consul &>/dev/null || {
+    pushd /usr/local/bin
+    [ -f consul_1.1.0_linux_amd64.zip ] || {
+        wget https://releases.hashicorp.com/consul/1.1.0/consul_1.1.0_linux_amd64.zip
+    }
+    unzip consul_1.1.0_linux_amd64.zip
+    chmod +x consul
+    popd
+}
+
+which /usr/local/go &>/dev/null || {
+    mkdir -p /tmp/go_src
+    pushd /tmp/go_src
+    [ -f go1.10.2.linux-amd64.tar.gz ] || {
+        wget -nv https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
+    }
+    tar -C /usr/local -xzf go1.10.2.linux-amd64.tar.gz
+    popd
+    rm -rf /tmp/go_src
+    echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+}
+
+
 # Reboot with the new kernel
 shutdown -r now
 sleep 60
