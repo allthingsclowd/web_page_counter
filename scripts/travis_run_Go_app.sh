39c48adc020d22f3d72e6a66dcb3bfafa007c3e1
Graham Land <graham@allthingscloud.eu>
2018-06-13 08:23:40 +0100

    Adding travis manual tests

--- scripts/travis_run_Go_app.sh
+++ scripts/travis_run_Go_app.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env bash
 go build main.go
 ./main &
+
+page_hit_counter=`lynx --dump http://localhost:9090 | awk 'NR>2{ print $1 }'`
+next_page_hit_counter=`lynx --dump http://localhost:9090 | awk 'NR>2{ print $1 }'`
+if (( next_page_hit_counter > page_hit_counter )); then
+ exit 0
+else
+ exit 1
+fi
