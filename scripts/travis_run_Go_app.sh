1cf90b15b9fb22629419abfca9ab315a7b48a598
Graham Land <graham@allthingscloud.eu>
2018-06-13 11:23:52 +0100

    fixed master - will work from branches in the future

--- scripts/travis_run_Go_app.sh
+++ scripts/travis_run_Go_app.sh
@@ -6,7 +6,7 @@ page_hit_counter=`lynx --dump http://localhost:8080 | awk 'NR>2{ print $1 }'`
 echo $page_hit_counter
 next_page_hit_counter=`lynx --dump http://localhost:8080 | awk 'NR>2{ print $1 }'`
 echo $next_page_hit_counter
-if (( next_page_hit_counter < page_hit_counter )); then
+if (( next_page_hit_counter > page_hit_counter )); then
  echo "Successful Page Hit Update"
  exit 0
 else
