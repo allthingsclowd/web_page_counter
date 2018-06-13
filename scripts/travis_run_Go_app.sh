f1e6f38c6fc45c227957159650cad85c30177efd
Graham Land <graham@allthingscloud.eu>
2018-06-13 09:09:43 +0100

    testing failure

--- scripts/travis_run_Go_app.sh
+++ scripts/travis_run_Go_app.sh
@@ -3,9 +3,13 @@ go build main.go
 ./main &
 
 page_hit_counter=`lynx --dump http://localhost:8080 | awk 'NR>2{ print $1 }'`
+echo $page_hit_counter
 next_page_hit_counter=`lynx --dump http://localhost:8080 | awk 'NR>2{ print $1 }'`
-if (( next_page_hit_counter > page_hit_counter )); then
+echo $next_page_hit_counter
+if (( next_page_hit_counter < page_hit_counter )); then
+ echo "Successful Page Hit Update"
  exit 0
 else
+ echo "Failed Page Hit Update"
  exit 1
 fi
