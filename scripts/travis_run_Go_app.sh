76f653400ae5d42c3b9dc2ade98fb91d1cff8b72
Graham Land <graham@allthingscloud.eu>
2018-06-13 08:24:33 +0100

    wip

--- scripts/travis_run_Go_app.sh
+++ scripts/travis_run_Go_app.sh
@@ -2,8 +2,8 @@
 go build main.go
 ./main &
 
-page_hit_counter=`lynx --dump http://localhost:9090 | awk 'NR>2{ print $1 }'`
-next_page_hit_counter=`lynx --dump http://localhost:9090 | awk 'NR>2{ print $1 }'`
+page_hit_counter=`lynx --dump http://localhost:8080 | awk 'NR>2{ print $1 }'`
+next_page_hit_counter=`lynx --dump http://localhost:8080 | awk 'NR>2{ print $1 }'`
 if (( next_page_hit_counter > page_hit_counter )); then
  exit 0
 else
