88d32497a6a8769229721d41ad665407fb7642f5
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-06-19 12:48:02 +0200

    test run consul on travis

--- scripts/install_consul.sh
+++ scripts/install_consul.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-source /usr/local/bootstrap/var.env
-
 IFACE=`route -n | awk '$1 == "192.168.2.0" {print $8}'`
 CIDR=`ip addr show ${IFACE} | awk '$2 ~ "192.168.2" {print $2}'`
 IP=${CIDR%%/24}
+
+if [ -d /vagrant ]; then
+  LOG="/vagrant/consul_${HOSTNAME}.log"
+else
+  LOG="consul.log"
+fi
 
 # check for consul hostname or travis => server
 if [[ "${HOSTNAME}" =~ "consul" ]] || [ "${TRAVIS}" == "true" ]; then
   echo server
   /usr/local/bin/consul members 2>/dev/null || {
-    /usr/local/bin/consul agent -server -ui -client=0.0.0.0 -bind=${IP} -data-dir=/usr/local/consul -bootstrap-expect=1 >/vagrant/consul_${HOSTNAME}.log &
+    /usr/local/bin/consul agent -server -ui -client=0.0.0.0 -bind=${IP} -data-dir=/usr/local/consul -bootstrap-expect=1 >${LOG} &
     sleep 5
     # upload vars to consul kv
 
@@ -28,7 +32,7 @@ if [[ "${HOSTNAME}" =~ "consul" ]] || [ "${TRAVIS}" == "true" ]; then
 else
   echo agent
   /usr/local/bin/consul members 2>/dev/null || {
-    /usr/local/bin/consul agent -client=0.0.0.0 -bind=${IP} -data-dir=/usr/local/consul -join=${CONSUL_IP} >/vagrant/consul_${HOSTNAME}.log &
+    /usr/local/bin/consul agent -client=0.0.0.0 -bind=${IP} -data-dir=/usr/local/consul -join=${CONSUL_IP} >${LOG} &
     sleep 10
   }
 fi
