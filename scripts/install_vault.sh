0662701320b929faf894bd3aaa472023c8fdc302
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-06-19 12:12:38 +0200

    some tweaks

--- scripts/install_vault.sh
+++ scripts/install_vault.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+IFACE=`route -n | awk '$1 == "192.168.2.0" {print $8}'`
+CIDR=`ip addr show ${IFACE} | awk '$2 ~ "192.168.2" {print $2}'`
+IP=${CIDR%%/24}
+
 which /usr/local/bin/vault &>/dev/null || {
     pushd /usr/local/bin
     [ -f vault_0.10.2_linux_amd64.zip ] || {
@@ -18,9 +22,9 @@ killall vault &>/dev/null
 #lets delete old consul storage
 consul kv delete -recurse vault
 
-/usr/local/bin/vault server  -dev -dev-listen-address=192.168.2.10:8200 -config=/usr/local/bootstrap/conf/vault.hcl >/vagrant/vault_${HOSTNAME}.log &
+/usr/local/bin/vault server  -dev -dev-listen-address=${IP}:8200 -config=/usr/local/bootstrap/conf/vault.hcl &>/vagrant/vault_${HOSTNAME}.log &
 sleep 3
-VAULT_ADDR='http://192.168.2.10:8200' vault kv put secret/hello value=world
-VAULT_ADDR='http://192.168.2.10:8200' vault kv get secret/hello
+VAULT_ADDR="http://${IP}:8200" vault kv put secret/hello value=world
+VAULT_ADDR="http://${IP}:8200" vault kv get secret/hello
 
 echo vault started
