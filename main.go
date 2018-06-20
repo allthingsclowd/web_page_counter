db92c476b9112a23e3b3d0e115a124c1b9c933ec
Graham Land <graham@allthingscloud.eu>
2018-06-20 15:27:28 +0100

    refactored

--- main.go
+++ main.go
@@ -7,18 +7,17 @@ import (
 	"html/template"
 	"github.com/go-redis/redis"
 	"os"
-	"fmt"
 	"strings"
 	"github.com/hashicorp/consul/api"
 	"strconv"
 )
 
 var templates *template.Template
-var client *redis.Client
+var redisClient *redis.Client
 var redisMaster string
 var redisPassword string
 var goapphealth = "GOOD"
-
+var consulClient *api.Client
 
 func main() {
 	redisMaster, redisPassword = redisInit()
@@ -26,20 +25,20 @@ func main() {
 	if (redisMaster == "0") || (redisPassword == "0") {
 
 		log.Printf("Check the Consul service is running")
-		goapphealth = "Check the Consul service is running"
+		goapphealth = "NOTGOOD"
 
 	} else {
 
-		client = redis.NewClient(&redis.Options{
+		redisClient = redis.NewClient(&redis.Options{
 			Addr:     redisMaster,
 			Password: redisPassword,
 			DB:       0,  // use default DB
 		})
 		
-		_, err := client.Ping().Result()
+		_, err := redisClient.Ping().Result()
 		if err != nil {
 			log.Printf("Failed to ping Redis: %v. Check the Redis service is running", err)
-			goapphealth="Check Redis Service is running"
+			goapphealth="NOTGOOD"
 		}
 	}
 	templates = template.Must(template.ParseGlob("templates/*.html"))
@@ -52,7 +51,7 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	pagehits, err := client.Incr("pagehits").Result()
+	pagehits, err := redisClient.Incr("pagehits").Result()
 	if err != nil {
 		log.Printf("Failed to increment page counter: %v. Check the Redis service is running", err)
 		goapphealth="REDIS PAGECOUNT FAILURE"
@@ -69,45 +68,71 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	
 }
 
+func getConsulKV(consulClient api.Client, key string) string {
+	
+	// Get a handle to the KV API
+	kv := consulClient.KV()
+
+	consulKey := "development/"+key
+
+	appVar, _, err := kv.Get(consulKey, nil)
+	if err != nil {
+		log.Printf("Failed to read key value %v - Please ensure key value exists in consul : e.g. consul kv get %v >> %v",key,key, err)
+		appVar, ok := os.LookupEnv(key)
+		if ok {
+			return appVar
+		}
+		log.Printf("Failed to read environment variable %v - Please ensure %v variable is set >> %v",key,key, err)
+		return "FAIL"
+
+	}
+
+	return string(appVar.Value)
+}
+
+func getConsulSVC(consulClient api.Client, key string) string {
+	
+	var serviceDetail strings.Builder
+	// get handle to catalog service api
+	sd := consulClient.Catalog()
+
+	myService, _, err := sd.Service(key, "", nil)
+	if err != nil {
+		log.Printf("Failed to discover Redis Service : e.g. curl http://localhost:8500/v1/catalog/service/redis >> %v", err)
+		return "0:0"
+	}
+	serviceDetail.WriteString(string(myService[0].Address))
+	serviceDetail.WriteString(":")
+	serviceDetail.WriteString(strconv.Itoa(myService[0].ServicePort))
+
+	return serviceDetail.String()
+}
+	
+
 func redisInit() (string, string) {
 	
-	var Master strings.Builder
-	var Password string
+	var redisService string
+	var redisPassword string
 	
 	// Get a new client
-	client, err := api.NewClient(api.DefaultConfig())
+	consulClient, err := api.NewClient(api.DefaultConfig())
 	if err !=nil {
 		log.Printf("Failed to contact consul - Please ensure both local agent and remote server are running : e.g. consul members >> %v", err)
 		return "0", "0"
 	}
-
-	// Get a handle to the KV API
-    kv := client.KV()
-
-	redisPasswordkvp, _, err := kv.Get("development/REDIS_MASTER_PASSWORD", nil)
-	if err != nil {
-		log.Printf("Failed to read key value <development/REDIS_MASTER_PASSWORD> - Please ensure key value exists : e.g. consul kv get development/REDIS_MASTER_PASSWORD >> %v", err)
-		Password = os.Getenv("REDIS_MASTER_PASSWORD")
-		return "0", "0"
+	redisPassword = getConsulKV(*consulClient, "REDIS_MASTER_PASSWORD")
+	redisService = getConsulSVC(*consulClient, "redis")
+	if redisService == "0:0" {
+		var serviceDetail strings.Builder
+		redisHost := getConsulKV(*consulClient, "REDIS_MASTER_IP")
+		redisPort := getConsulKV(*consulClient, "REDIS_HOST_PORT")
+		serviceDetail.WriteString(redisHost)
+		serviceDetail.WriteString(":")
+		serviceDetail.WriteString(redisPort)
+		redisService = serviceDetail.String()
 	}
-	Password = string(redisPasswordkvp.Value)
-	fmt.Println(string(redisPasswordkvp.Value))
 	
-	// get handle to catalog service api
-	sd := client.Catalog()
-
-	redisService, _, err := sd.Service("redis", "primary", nil)
-	if err != nil {
-		log.Printf("Failed to discover Redis Service : e.g. curl http://localhost:8500/v1/catalog/service/redis >> %v", err)
-		return "0", "0"
-	}
-	Master.WriteString(string(redisService[0].Address))
-	Master.WriteString(":")
-	Master.WriteString(strconv.Itoa(redisService[0].ServicePort))
-	fmt.Println(Master.String())
-	
-
-	return Master.String(), Password
+	return redisService, redisPassword
 
 }
 
