28fa56b2b44880644eabcfc6012dfc38eda5d28e
Graham Land <graham@allthingscloud.eu>
2018-06-21 22:33:33 +0100

    added delay before app start

--- scripts/install_Go_app.sh
+++ scripts/install_Go_app.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 source /usr/local/bootstrap/var.env
 
-# may add this to base image in future
-apt-get install -y lynx
-
 IFACE=`route -n | awk '$1 == "192.168.2.0" {print $8}'`
 CIDR=`ip addr show ${IFACE} | awk '$2 ~ "192.168.2" {print $2}'`
 IP=${CIDR%%/24}
@@ -15,23 +12,23 @@ else
 fi
 
 # Idempotency hack - if this file exists don't run the rest of the script
-if [ -f "$HOME/vagrant_go_user" ]; then
+if [ -f "${HOME}/vagrant_go_user" ]; then
     exit 0
 fi
 
-touch $HOME/vagrant_go_user
-SOURCE=$HOME/code/go/src/github.com/allthingsclowd/golang_web_page_counter
-mkdir -p $SOURCE
-echo "export GOPATH=$HOME/code/go" >> $HOME/.profile
+touch ${HOME}/vagrant_go_user
+SOURCE=${HOME}/code/go/src/github.com/allthingsclowd/golang_web_page_counter
+mkdir -p ${SOURCE}
+echo "export GOPATH=${HOME}/code/go" >> ${HOME}/.profile
 source $HOME/.profile
-cp -r /vagrant/. $SOURCE
-cd $SOURCE
+cp -r /vagrant/. ${SOURCE}
+cd ${SOURCE}
 go get ./...
 go build main.go
 echo "about to run go app"
+sleep 20
 ./main >/vagrant/go_app_start_up_${HOSTNAME}.log &
 echo " app should be started"
-sleep 20
 
 # copy a consul service definition directory
  mkdir -p /etc/consul.d
