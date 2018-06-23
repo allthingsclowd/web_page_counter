a503426141e9c4a97cf730f6e8fd4cdae83b1928
Graham Land <graham@allthingscloud.eu>
2018-06-23 13:46:31 +0100

    here we go

--- scripts/consul_goapp_verify.sh
+++ scripts/consul_goapp_verify.sh
@@ -6,8 +6,9 @@ set -e
 echo "running consul goapp client health test"
 app_health="NOTGOOD"
 app_health=`lynx --dump http://$GO_DEV_IP:$GO_HOST_PORT/health`
+echo $app_health
 
-if (( app_health == "GOOD" )); then
+if [ ${app_health} == "GOOD" ]; then
  echo "Application Services GOOD"
  exit 0
 else
