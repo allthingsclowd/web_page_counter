3a4a7102c5703e0690865fa296e1cb64301b9ed6
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-06-25 12:59:31 +0200

    move install consul-template and use killall -1 consul for reload

--- scripts/install_webserver.sh
+++ scripts/install_webserver.sh
@@ -12,36 +12,23 @@ else
 fi
 
 # Idempotency hack - if this file exists don't run the rest of the script
-if [ -f "/var/vagrant_web_server" ]; then
-    exit 0
-fi
+#if [ -f "/var/vagrant_web_server" ]; then
+#    exit 0
+#fi
 
-touch /var/vagrant_web_server
+#touch /var/vagrant_web_server
 
 # remove nginx default website
 sudo rm -f /etc/nginx/sites-enabled/default
 
-[ -f /usr/local/bin/consul-template ] &>/dev/null || {
-    pushd /usr/local/bin
-    [ -f consul-template_0.19.5_linux_amd64.zip ] || {
-        sudo wget https://releases.hashicorp.com/consul-template/0.19.5/consul-template_0.19.5_linux_amd64.zip
-    }
-    sudo unzip consul-template_0.19.5_linux_amd64.zip
-    sudo chmod +x consul-template
-    popd
-}
-
 # copy a consul service definition directory
- sudo mkdir -p /etc/consul.d
- sudo cp -p /usr/local/bootstrap/conf/consul.d/webtier.json /etc/consul.d/webtier.json
- # lets kill past instance
- sudo killall consul &>/dev/null
- sleep 5
- # start restart with config dir
- sudo /usr/local/bin/consul agent -client=0.0.0.0 -bind=${IP} -config-dir=/etc/consul.d -enable-script-checks=true -data-dir=/usr/local/consul -join=${CONSUL_IP} >${LOG} &
- sleep 5
+sudo mkdir -p /etc/consul.d
+sudo cp -p /usr/local/bootstrap/conf/consul.d/webtier.json /etc/consul.d/webtier.json
 
- sudo /usr/local/bin/consul-template \
+# make consul reload conf
+sudo killall -1 consul
+
+sudo /usr/local/bin/consul-template \
      -consul-addr=$CONSUL_IP:8500 \
      -template "/usr/local/bootstrap/conf/nginx.ctpl:/etc/nginx/conf.d/goapp.conf:service nginx reload" &
      
