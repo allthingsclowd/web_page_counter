a0a9bb6242f3e239374faf3600ef1db4cee9c884
Graham Land <graham@allthingscloud.eu>
2018-06-29 09:18:12 +0100

    initial vagrant test working

--- scripts/install_Go_app.sh
+++ scripts/install_Go_app.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 source /usr/local/bootstrap/var.env
 
-IFACE=`route -n | awk '$1 == "192.168.2.0" {print $8}'`
+IFACES=`route -n | awk '$1 == "192.168.2.0" {print $8}'`
+IFACE=`echo $IFACES | awk '{ print $1 }'`
 CIDR=`ip addr show ${IFACE} | awk '$2 ~ "192.168.2" {print $2}'`
 IP=${CIDR%%/24}
 
@@ -16,6 +17,11 @@ if [ -f "${HOME}/vagrant_go_user" ]; then
     exit 0
 fi
 
+# copy a consul service definition directory
+mkdir -p /etc/consul.d
+chmod +x /usr/local/bootstrap/scripts/consul_build_go_app_service.sh
+apt-get install -y jq
+
 export GOPATH=$HOME/gopath
 export PATH=$HOME/gopath/bin:$PATH
 mkdir -p $HOME/gopath/src/github.com/allthingsclowd/golang_web_page_counter
@@ -24,30 +30,24 @@ cd $HOME/gopath/src/github.com/allthingsclowd/golang_web_page_counter
 go get -t -v ./...
 go build main.go
 echo $1
-./main -port=$1 >>/vagrant/goapp_${HOSTNAME}.log &
-
-echo "debug delay - sleep 5"
-sleep 5
-
-# copy a consul service definition directory
-mkdir -p /etc/consul.d
-chmod +x /usr/local/bootstrap/scripts/consul_build_go_app_service.sh
-apt install -y jq
-
-if [ $LISTENER_COUNT -gt 1 ]; then
-  COUNTER=1
-  let LISTENER_COUNT++
-
-  while [ $COUNTER -lt $LISTENER_COUNT ]; do
-    HOSTURL="http://${IP}:808${COUNTER}/health"
-    /usr/local/bootstrap/scripts/consul_build_go_app_service.sh /usr/local/bootstrap/conf/consul.d/goapp.json /etc/consul.d/goapp${COUNTER}.json $HOSTURL 808${COUNTER}
-    let COUNTER=COUNTER+1 
+DEFAULTPORT=0
+if [ `echo $IFACES | wc -w` -gt 1 ]; then
+  DEFAULTPORT=1
+  for interface in $IFACES;
+  do
+    CIDR=`ip addr show $interface | awk '$2 ~ "192.168.2" {print $2}'`
+    IFACEIP=${CIDR%%/24}
+    ./main -port=808$DEFAULTPORT -ip=$IFACEIP >>/vagrant/goapp_${HOSTNAME}.log &
+    HOSTURL="http://${IFACEIP}:808${DEFAULTPORT}/health"
+    /usr/local/bootstrap/scripts/consul_build_go_app_service.sh /usr/local/bootstrap/conf/consul.d/goapp.json /etc/consul.d/goapp808${DEFAULTPORT}.json $HOSTURL 808${DEFAULTPORT}
+    let DEFAULTPORT++
   done
 else
-  COUNTER=0
-  HOSTURL="http://${IP}:808${COUNTER}/health"
-  /usr/local/bootstrap/scripts/consul_build_go_app_service.sh /usr/local/bootstrap/conf/consul.d/goapp.json /etc/consul.d/goapp${COUNTER}.json $HOSTURL 808${COUNTER}
+  ./main -port=808$DEFAULTPORT -ip=$IP >>/vagrant/goapp_${HOSTNAME}.log &
+  /usr/local/bootstrap/scripts/consul_build_go_app_service.sh /usr/local/bootstrap/conf/consul.d/goapp.json /etc/consul.d/goapp808${DEFAULTPORT}.json $HOSTURL 808${DEFAULTPORT}
 fi
+
+sleep 5
 
 # lets kill past instance
 killall consul &>/dev/null
