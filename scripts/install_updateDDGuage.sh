54c2b54aa3f145379e6c862b71b9026c70d44648
Graham Land <graham@allthingscloud.eu>
2018-07-08 22:26:32 +0100

    cronjob added

---
+++ scripts/install_updateDDGuage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source /usr/local/bootstrap/var.env
+
+sudo mkdir /usr/local/datadog
+
+export GOPATH=$HOME/gopath
+export PATH=$HOME/gopath/bin:$PATH
+go get github.com/allthingsclowd/updateDDGuage
+cd $HOME/gopath/src/github.com/allthingsclowd/updateDDGuage
+cp metric.json /usr/local/datadog/.
+go build -o updateDDGuage main.go
+chmod +x updateDDGuage
+cp updateDDGuage /usr/local/bin/.
+
+
+
---
+++ scripts/updateBackendCount.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SERVICES=`consul catalog services | grep goapp`
+
+SERVICECOUNT=0
+for backend in $SERVICES;
+do 
+    BCOUNT=`consul catalog nodes -service=$backend  | awk '{count++} ;END{print count-1}'`;
+    SERVICECOUNT=$(($SERVICECOUNT+$BCOUNT))
+done
+echo "Total Backends: " $SERVICECOUNT;
+
+CURRENTSC=`cat /usr/local/datadog/metric.json | jq .[0].backendCount`
+
+if [ $CURRENTSC -eq $SERVICECOUNT ]; then
+    echo "No updates required"
+    exit 0
+else
+    echo "Updates in progess"
+    jq ".[0].backendCount = $SERVICECOUNT" /usr/local/bootstrap/conf/metric.json > /usr/local/datadog/metricupdate.json
+    mv /usr/local/datadog/metricupdate.json /usr/local/datadog/metric.json
+fi
