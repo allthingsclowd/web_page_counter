84ea176b5564f1fcfd38ca03be6d647b3d63d9e6
Graham Land <graham@allthingscloud.eu>
2018-07-10 10:08:34 +0100

    localhost change dd

--- scripts/updateBackendCount.sh
+++ scripts/updateBackendCount.sh
@@ -5,7 +5,6 @@ metric=`grep '192.168.2' /etc/nginx/conf.d/goapp.conf | wc -l`
 name="backendtotal"
 
 echo "${name}:${metric}|g|#web"
-echo -n "${name}:${metric}|g|#web" >/dev/udp/localhost/8125
+echo -n "${name}:${metric}|g|#web" >/dev/udp/127.0.0.1/8125
 
-echo -n "bananatest:42|g|#web" >/dev/udp/localhost/8125
 
