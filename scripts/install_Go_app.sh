b93211512a26d3ff093bfd90888d11978ebadfc9
Graham Land <graham@allthingscloud.eu>
2018-07-13 09:01:46 +0100

    tidy

--- scripts/install_Go_app.sh
+++ scripts/install_Go_app.sh
@@ -2,16 +2,18 @@
 
 source /usr/local/bootstrap/var.env
 
-export GOPATH=$HOME/gopath
-export PATH=$HOME/gopath/bin:$PATH
-mkdir -p $HOME/gopath/src/github.com/allthingsclowd/web_page_counter
-cp -r /usr/local/bootstrap/. $HOME/gopath/src/github.com/allthingsclowd/web_page_counter/
-cd $HOME/gopath/src/github.com/allthingsclowd/web_page_counter
-go get -t -v ./...
-go build main.go
-
-mkdir -p /usr/local/page_counter
-killall main 2>/dev/null
-cp -ap main templates /usr/local/page_counter
+# check goapp binary
+[ -f /usr/local/bin/webcounter ] &>/dev/null || {
+    export GOPATH=$HOME/gopath
+    export PATH=$HOME/gopath/bin:$PATH
+    mkdir -p $HOME/gopath/src/github.com/allthingsclowd/web_page_counter
+    cp -r /usr/local/bootstrap/. $HOME/gopath/src/github.com/allthingsclowd/web_page_counter/
+    cd $HOME/gopath/src/github.com/allthingsclowd/web_page_counter
+    go get -t -v ./...
+    go build -o webcounter main.go
+    chmod +x webcounter
+    cp webcounter /usr/local/bin/.
+    cp -r /usr/local/bootstrap/templates /usr/local/bin/.
+}
 
 nomad job run /usr/local/bootstrap/nomad_job.hcl
