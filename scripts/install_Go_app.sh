155b23839c76d70d8699238b1ce893e2aeefc2b0
Graham Land <graham@allthingscloud.eu>
2018-08-01 11:37:03 +0100

    modified to use releases

--- scripts/install_Go_app.sh
+++ scripts/install_Go_app.sh
@@ -3,20 +3,17 @@ set -x
 
 source /usr/local/bootstrap/var.env
 
-# check goapp binary
-export GOPATH=$HOME/gopath
-export PATH=$HOME/gopath/bin:$PATH
-mkdir -p $HOME/gopath/src/github.com/allthingsclowd/web_page_counter
-cp -r /usr/local/bootstrap/. $HOME/gopath/src/github.com/allthingsclowd/web_page_counter/
-cd $HOME/gopath/src/github.com/allthingsclowd/web_page_counter
-go get -t -v ./...
-go build -o webcounter main.go
-chmod +x webcounter
+# download binary and template file from latest release
+curl -s https://api.github.com/repos/allthingsclowd/web_page_counter/releases/latest \
+| grep "browser_download_url" \
+| cut -d : -f 2,3 \
+| tr -d \" | wget -i -
 
 nomad job stop peach &>/dev/null
 killall webcounter &>/dev/null
 cp webcounter /usr/local/bin/.
-cp -r /usr/local/bootstrap/templates /usr/local/bin/.
+cp *.html /usr/local/bin/templates.
+
 cp /usr/local/bootstrap/scripts/consul_goapp_verify.sh /usr/local/bin/.
 
 nomad job run /usr/local/bootstrap/nomad_job.hcl
