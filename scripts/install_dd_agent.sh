bd643e40ea2dda7388de685734fb8de2885bcd34
Graham Land <graham@allthingscloud.eu>
2018-09-23 23:37:02 +0100

    test new packer build

--- scripts/install_dd_agent.sh
+++ scripts/install_dd_agent.sh
@@ -3,11 +3,5 @@ set -x
 
 source /usr/local/bootstrap/var.env
 
-# install this package in base image in the future
-which curl &>/dev/null || {
-  sudo apt-get update
-  sudo apt-get install -y curl
-}
-
 # install datadog agent
 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
