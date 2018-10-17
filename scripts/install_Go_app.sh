cbf26da345fa421fd537974d720f9fe924d193ea
Graham Land <graham@allthingscloud.eu>
2018-10-17 07:35:59 +0100

    Added Proxy StartUp

--- scripts/install_Go_app.sh
+++ scripts/install_Go_app.sh
@@ -3,6 +3,9 @@ set -x
 
 source /usr/local/bootstrap/var.env
 
+# start consul proxy for mesh services
+sudo consul connect proxy -service web-page-counter-proxy -upstream redis:8878 &
+
 # download binary and template file from latest release
 curl -s https://api.github.com/repos/allthingsclowd/web_page_counter/releases/latest \
 | grep "browser_download_url" \
