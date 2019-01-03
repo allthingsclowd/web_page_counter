0dceed39c17d5dbef46fc01a999a3c3ce8bc3e69
Graham Land <graham@allthingscloud.eu>
2019-01-03 19:43:22 +0000

    removed type and fixed vbox guest addition installations

--- scripts/packer_install_guest_additions.sh
+++ scripts/packer_install_guest_additions.sh
@@ -8,8 +8,18 @@ apt-get -y install build-essential linux-headers-$(uname -r)
 service virtualbox-ose-guest-utils stop
 rmmod vboxguest
 apt-get purge -y virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
-apt-get install -y dkms virtualbox-guest-dkms linux-headers-virtual
+apt-get install -y dkms
 
-echo 'Removed old Vbox support'
+# Install the VirtualBox guest additions
+ls -al
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+VBOX_ISO=VBoxGuestAdditions.iso
+mount -o loop $VBOX_ISO /mnt
+yes|sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+#Cleanup VirtualBox
+rm $VBOX_ISO
+
 shutdown -r now
 sleep 60
