9d41c1e254fe78980107797f302094040cec99af
Graham Land <graham@allthingscloud.eu>
2019-03-04 23:53:00 +0000

    tidied

--- terraform/output.tf
+++ terraform/output.tf
@@ -11,15 +11,20 @@ locals {
 
       Open a browser on the following URLs to access each service
 
-      WebPageCounter Application FrontEnd -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9091
-      WebPageCounter Application BackEnd  -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9090
+      WebPageCounter Application FrontEnd (public)  -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9091
+      WebPageCounter Application BackEnd (public)   -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9090
+      WebPageCounter Application FrontEnd -   http://${azurerm_network_interface.wpcproxynic.private_ip_address}:9091
+      WebPageCounter Application BackEnd  -   http://${azurerm_network_interface.wpcproxynic.private_ip_address}:9090      
+      Nomad Portal  (public)  -   http://${data.azurerm_public_ip.hashicorp_portals}:4646
+      Vault Portal  (public)  -   http://${data.azurerm_public_ip.hashicorp_portals}:8200
+      Consul Portal (public)  -   https://${data.azurerm_public_ip.hashicorp_portals}:8321
       Nomad Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:4646
       Vault Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:8200
-      Consul Portal   -   https://${azurerm_network_interface.wpcnic.private_ip_address}:8321
-      (self-signed certificates - ../certificate-config directory)
+      Consul Portal   -   https://${azurerm_network_interface.wpcnic.private_ip_address}:8321      
+      (self-signed certificates located in ../certificate-config directory)
 
       Vault Password  -   reallystrongpassword
-      Consul ACL  -   Navigate to Vault to locate the consul ACL token then use it to login to the Consul portal
+      Consul ACL      -   Navigate to Vault to locate the consul ACL token then use it to login to the Consul portal
 
 
 
