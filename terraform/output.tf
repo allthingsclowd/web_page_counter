2d5271b42386e2e4a60bf263dbc20feaae9e52a5
Graham Land <graham@allthingscloud.eu>
2019-03-04 23:29:13 +0000

    tidied

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,21 +5,24 @@
 locals {
   hashistackdemo = <<AZUREDEMO
 
-Welcome to The TAM HashiStack demo
+      Welcome to The TAM HashiStack demo
         
-            on Azure
+                on Azure
 
-Open a browser on the following URLs to access each service
+      Open a browser on the following URLs to access each service
 
-    WebPageCounter Application FrontEnd -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9091
-    WebPageCounter Application BackEnd  -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9090
-    Nomad Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:4646
-    Vault Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:8200
-    Consul Portal   -   https://${azurerm_network_interface.wpcnic.private_ip_address}:8321
-    (self-signed certificates - ../certificate-config directory)
+      WebPageCounter Application FrontEnd -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9091
+      WebPageCounter Application BackEnd  -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9090
+      Nomad Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:4646
+      Vault Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:8200
+      Consul Portal   -   https://${azurerm_network_interface.wpcnic.private_ip_address}:8321
+      (self-signed certificates - ../certificate-config directory)
 
-    Vault Password  -   reallystrongpassword
-    Consul ACL  -   Navigate to Vault to locate the consul ACL token then use it to login to the Consul portal
+      Vault Password  -   reallystrongpassword
+      Consul ACL  -   Navigate to Vault to locate the consul ACL token then use it to login to the Consul portal
+
+
+
 
 AZUREDEMO
 
