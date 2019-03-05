e7b68cf861a72352a8814874103420c9226c2695
Graham Land <graham@allthingscloud.eu>
2019-03-05 00:00:27 +0000

    tidied

--- terraform/output.tf
+++ terraform/output.tf
@@ -15,9 +15,9 @@ locals {
       WebPageCounter Application BackEnd (public)   -   http://${data.azurerm_public_ip.web_front_end.ip_address}:9090
       WebPageCounter Application FrontEnd -   http://${azurerm_network_interface.wpcproxynic.private_ip_address}:9091
       WebPageCounter Application BackEnd  -   http://${azurerm_network_interface.wpcproxynic.private_ip_address}:9090      
-      Nomad Portal  (public)  -   http://${data.azurerm_public_ip.hashicorp_portals}:4646
-      Vault Portal  (public)  -   http://${data.azurerm_public_ip.hashicorp_portals}:8200
-      Consul Portal (public)  -   https://${data.azurerm_public_ip.hashicorp_portals}:8321
+      Nomad Portal  (public)  -   http://${data.azurerm_public_ip.hashicorp_portals.ip_address}:4646
+      Vault Portal  (public)  -   http://${data.azurerm_public_ip.hashicorp_portals.ip_address}:8200
+      Consul Portal (public)  -   https://${data.azurerm_public_ip.hashicorp_portals.ip_address}:8321
       Nomad Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:4646
       Vault Portal    -   http://${azurerm_network_interface.wpcnic.private_ip_address}:8200
       Consul Portal   -   https://${azurerm_network_interface.wpcnic.private_ip_address}:8321      
