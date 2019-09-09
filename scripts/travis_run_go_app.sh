78dea25871c6e2f3c170f8d363b396e520c70fe9
Graham Land <graham@allthingscloud.eu>
2019-09-09 16:10:46 +0100

    prepping vmware image

--- scripts/travis_run_go_app.sh
+++ scripts/travis_run_go_app.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 set -x
 
+echo "Start Golang installation"
+which /usr/local/go &>/dev/null || {
+    echo "Create a temporary directory"
+    sudo mkdir -p /tmp/go_src
+    pushd /tmp/go_src
+    [ -f go${golang_version}.linux-amd64.tar.gz ] || {
+        echo "Download Golang source"
+        sudo wget -qnv https://dl.google.com/go/go${golang_version}.linux-amd64.tar.gz
+    }
+    
+    echo "Extract Golang source"
+    sudo tar -C /usr/local -xzf go${golang_version}.linux-amd64.tar.gz
+    popd
+    echo "Remove temporary directory"
+    sudo rm -rf /tmp/go_src
+    echo "Edit profile to include path for Go"
+    echo "export PATH=$PATH:/usr/local/go/bin" | sudo tee -a /etc/profile
+    echo "Ensure others can execute the binaries"
+    sudo chmod -R +x /usr/local/go/bin/
+    cat /etc/profile
+    source /etc/profile
+
+    go version
+
+}
+
 # delayed added to ensure consul has started on host - intermittent failures
 sleep 2
 
@@ -9,7 +35,7 @@ export CONSUL_HTTP_TOKEN=${AGENTTOKEN}
 
 go get ./...
 go build -o webcounter main.go
-./webcounter -consulACL=${CONSUL_HTTP_TOKEN} -ip="0.0.0.0" -consulIp="192.168.9.120:8321" &
+./webcounter -consulACL=${CONSUL_HTTP_TOKEN} -ip="0.0.0.0" -consulIp="127.0.0.1:8321" &
 
 # delay added to allow webcounter startup
 sleep 2
