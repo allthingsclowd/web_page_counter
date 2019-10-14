0da0d88cea49d91839e4bddeb7b6fe96b99c6173
Graham Land <graham@allthingscloud.eu>
2019-10-14 15:12:11 +0100

    1

--- scripts/travis_run_go_app.sh
+++ scripts/travis_run_go_app.sh
@@ -1,8 +1,43 @@
 #!/usr/bin/env bash
 set -x
 
-go get -mod=vendor ./...
-go build -o webcounter -i main.go
-ls -al webcounter
+# delayed added to ensure consul has started on host - intermittent failures
+sleep 2
 
+AGENTTOKEN=`sudo VAULT_TOKEN=reallystrongpassword VAULT_ADDR="http://${LEADER_IP}:8200" vault kv get -field "value" kv/development/consulagentacl`
+export CONSUL_HTTP_TOKEN=${AGENTTOKEN}
+
+/usr/local/go/bin/go get ./...
+/usr/local/go/bin/go build -o webcounter -i main.go
+./webcounter -consulACL=${CONSUL_HTTP_TOKEN} -ip="0.0.0.0" -consulIp="127.0.0.1:8321" &
+
+# delay added to allow webcounter startup
+sleep 2
+
+ps -ef | grep webcounter 
+
+# check health
+echo "APPLICATION HEALTH"
+curl -s http://127.0.0.1:8314/health
+
+curl -s http://localhost:8080/health
+
+curl -s http://localhost:8080
+
+curl -s http://127.0.0.1:8080/health
+
+curl -s http://127.0.0.1:8080
+
+page_hit_counter=`lynx --dump http://127.0.0.1:8080`
+echo $page_hit_counter
+next_page_hit_counter=`lynx --dump http://127.0.0.1:8080`
+
+echo $next_page_hit_counter
+if (( next_page_hit_counter > page_hit_counter )); then
+ echo "Successful Page Hit Update"
+ exit 0
+else
+ echo "Failed Page Hit Update"
+ exit 1
+fi
 # The End
