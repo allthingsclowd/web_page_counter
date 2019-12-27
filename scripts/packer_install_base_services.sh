6189cd6ba35955a6cc0534fad00c75ad0d6a7e3c
Graham Land <graham@allthingscloud.eu>
2019-12-27 16:00:42 +0000

    added services to base image

---
+++ scripts/packer_install_base_services.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+create_service () {
+  if [ ! -f /etc/systemd/system/${1}.service ]; then
+    
+    create_service_user ${1}
+    
+    sudo tee /etc/systemd/system/${1}.service <<EOF
+### BEGIN INIT INFO
+# Provides:          ${1}
+# Required-Start:    $local_fs $remote_fs
+# Required-Stop:     $local_fs $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: ${1} agent
+# Description:       ${2}
+### END INIT INFO
+
+[Unit]
+Description=${2}
+Requires=network-online.target
+After=network-online.target
+ConditionFileNotEmpty=/etc/${1}.d/${1}.hcl
+StartLimitIntervalSec=60
+StartLimitBurst=3
+
+[Service]
+Type=notify
+User=${1}
+Group=${1}
+PIDFile=/var/run/${1}/${1}.pid
+PermissionsStartOnly=true
+ExecStartPre=-/bin/mkdir -p /var/run/${1}
+ExecStartPre=/bin/chown -R ${1}:${1} /var/run/${1}
+ExecStart=${3}
+ExecReload=/bin/kill -HUP ${MAINPID}
+KillMode=process
+KillSignal=SIGTERM
+Restart=on-failure
+RestartSec=2s
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl daemon-reload
+
+  fi
+
+}
+
+create_vault_service () {
+  if [ ! -f /etc/systemd/system/${1}.service ]; then
+    
+    create_service_user ${1}
+    
+    sudo tee /etc/systemd/system/${1}.service <<EOF
+### BEGIN INIT INFO
+# Provides:          ${1}
+# Required-Start:    $local_fs $remote_fs
+# Required-Stop:     $local_fs $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: ${1} agent
+# Description:       ${2}
+### END INIT INFO
+
+[Unit]
+Description=${2}
+Documentation=https://www.vaultproject.io/docs/
+Requires=network-online.target
+After=network-online.target
+ConditionFileNotEmpty=/etc/${1}.d/${1}.hcl
+StartLimitIntervalSec=60
+StartLimitBurst=3
+
+[Service]
+User=${1}
+Group=${1}
+ProtectSystem=full
+ProtectHome=read-only
+PrivateTmp=yes
+PrivateDevices=yes
+SecureBits=keep-caps
+AmbientCapabilities=CAP_IPC_LOCK
+Capabilities=CAP_IPC_LOCK+ep
+CapabilityBoundingSet=CAP_SYSLOG CAP_IPC_LOCK
+NoNewPrivileges=yes
+ExecStart=${3}
+ExecReload=/bin/kill --signal HUP $MAINPID
+KillMode=process
+KillSignal=SIGINT
+Restart=on-failure
+RestartSec=5
+TimeoutStopSec=30
+StartLimitInterval=60
+StartLimitIntervalSec=60
+StartLimitBurst=3
+LimitNOFILE=65536
+LimitMEMLOCK=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl daemon-reload
+
+  fi
+
+}
+
+create_service_user () {
+  
+  if ! grep ${1} /etc/passwd >/dev/null 2>&1; then
+    echo "Creating ${1} user to run the ${1} service"
+    sudo useradd --system --home /etc/${1}.d --shell /bin/false ${1}
+    sudo mkdir --parents /opt/${1} /usr/local/${1} /etc/${1}.d
+    sudo chown --recursive ${1}:${1} /opt/${1} /etc/${1}.d /usr/local/${1}
+  fi
+
+}
+
+create_consul_service () {
+    
+    create_service consul "HashiCorp Consul Server SD & KV Service" "/usr/local/bin/consul agent -server -log-level=debug -ui -client=0.0.0.0 -config-dir=/etc/consul.d -enable-script-checks=true -data-dir=/usr/local/consul -bootstrap-expect=1"
+    sudo systemctl disable consul
+}
+
+create_vault_service () {
+    
+    sudo setcap cap_ipc_lock=+ep /usr/local/bin/vault
+    create_vault_service vault "HashiCorp's Sercret Management Service" "/usr/local/bin/vault server -dev -dev-root-token-id="reallystrongpassword" -config=/etc/vault.d/vault.hcl"
+    sudo systemctl disable vault
+}
+
+create_nomad_service () {
+    
+    create_service nomad "HashiCorp's Nomad Server - A Modern Platform and Cloud Agnostic Scheduler" "/usr/local/bin/nomad agent -log-level=DEBUG -server -data-dir=/usr/local/nomad -bootstrap-expect=1 -config=/etc/nomad.d"
+    sudo systemctl disable nomad
+}
+
+create_envoy_service () {
+    
+    create_service envoy "Envoy Proxy Server" "/usr/bin/envoy"
+    sudo systemctl disable envoy
+}
+
+create_consul_service
+create_vault_service
+create_nomad_service
+create_envoy_service
