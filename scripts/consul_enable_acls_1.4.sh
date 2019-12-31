e030d49877071817693129fdc6bce24efaf26452
Graham Land <graham@allthingscloud.eu>
2019-12-31 13:31:22 +0000

    fixed nomad certs

--- scripts/consul_enable_acls_1.4.sh
+++ scripts/consul_enable_acls_1.4.sh
@@ -294,9 +294,9 @@ EOF
 consul {
   address = "127.0.0.1:8321"
   ssl       = true
-  ca_file   = "/etc/pki/tls/certs/consul-ca.pem"
-  cert_file = "/etc/pki/tls/certs/server.pem"
-  key_file  = "/etc/pki/tls/private/server-key.pem"
+  ca_file   = "/etc/nomad.d/pki/tls/certs/consul/consul-ca.pem"
+  cert_file = "/etc/nomad.d/pki/tls/certs/consul/server.pem"
+  key_file  = "/etc/nomad.d/pki/tls/private/consul/server-key.pem"
   token = "${CONSUL_HTTP_TOKEN}"
   }
 EOF
@@ -311,9 +311,9 @@ step9_configure_nomad() {
 consul {
   address = "127.0.0.1:8321"
   ssl       = true
-  ca_file   = "/etc/pki/tls/certs/consul-ca.pem"
-  cert_file = "/etc/pki/tls/certs/server.pem"
-  key_file  = "/etc/pki/tls/private/server-key.pem"
+  ca_file   = "/etc/nomad.d/pki/tls/certs/consul/consul-ca.pem"
+  cert_file = "/etc/nomad.d/pki/tls/certs/consul/server.pem"
+  key_file  = "/etc/nomad.d/pki/tls/private/consul/server-key.pem"
   token = "${AGENTTOKEN}"
   }
 EOF
