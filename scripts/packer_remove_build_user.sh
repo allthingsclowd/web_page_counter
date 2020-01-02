3526b75a82b0ebc3f3827c2eee3251dd45706dac
Graham Land <graham@allthingscloud.eu>
2020-01-02 16:44:41 +0000

    added missing ca pub file

--- scripts/packer_remove_build_user.sh
+++ scripts/packer_remove_build_user.sh
@@ -2,7 +2,7 @@
 
 if ! crontab -l | grep deluser; then
     echo 'Setting up crontab to delete user'
-    (crontab -l ; echo "@reboot deluser --remove-home vagrant")| crontab -
+    (crontab -l ; echo "@reboot /usr/sbin/deluser --force --remove-home --group vagrant")| crontab -
     crontab -l
 else
     echo 'Resetting crontab back to what was originally there...possibly blank'
