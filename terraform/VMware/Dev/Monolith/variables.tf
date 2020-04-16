113a5ef23456d00134c1d9b914466a9871dc739d
allthingsclowd <graham@allthingscloud.eu>
2020-04-16 22:56:53 +0100

    removed obsolete vars

--- terraform/VMware/Dev/Monolith/variables.tf
+++ terraform/VMware/Dev/Monolith/variables.tf
@@ -10,18 +10,9 @@ variable "vsphere_server" {
   description = "vCentre IP Address"
 }
 
-variable "ssh_certificate" {
-  description = "SSH Signed Certificate"
-}
-
-variable "ssh_private_key" {
-  description = "SSH Private Key"
-}
-
 variable "bastion_host" {
   description = "Bastion Host IP Address or DNS Name"
 }
-
 
 variable "bastion_port" {
   description = "Bastion Port Number"
